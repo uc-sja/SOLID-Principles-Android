54259cdb5dca6705a8130874ccfcb87187f8010d
Anil Deshpande <anildesh82@gmail.com>
2024-05-03 09:24:27 +0530

    Room 2 video

--- app/src/test/java/com/codetutor/countryinfoapp/ExampleUnitTest.kt
+++ app/src/test/java/com/codetutor/countryinfoapp/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.codetutor.countryinfoapp
 
+import com.codetutor.countryinfoapp.database.Converters
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -9,8 +10,27 @@ import org.junit.Test
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    private val converters = Converters<Any>()
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun testFromJsonStringToList() {
+        val jsonString = "[\"India\",\"USA\",\"UK\"]"
+        val expectedList = listOf("India", "USA", "UK")
+        val actualList = converters.fromJsonStringToList(jsonString)
+        assertEquals(expectedList, actualList)
+    }
+
+    @Test
+    fun testFromStringListToJson() {
+        val list = listOf("India", "USA", "UK")
+        val expectedJsonString = "[\"India\",\"USA\",\"UK\"]"
+        val actualJsonString = converters.fromStringListToJson(list)
+        assertEquals(expectedJsonString, actualJsonString)
     }
 }
