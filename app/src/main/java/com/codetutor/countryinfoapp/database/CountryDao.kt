8d59890b830299902e9992c7676650ea9bb89587
Anil Deshpande <anildesh82@gmail.com>
2024-05-07 01:01:37 +0530

    Video room 3

---
+++ app/src/main/java/com/codetutor/countryinfoapp/database/CountryDao.kt
@@ -0,0 +1,37 @@
+package com.codetutor.countryinfoapp.database
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.codetutor.countryinfoapp.data.Country
+
+@Dao
+interface CountryDao {
+    // Add your queries here
+
+    //select
+    @Query("SELECT * from Country")
+    suspend fun getAllCountries(): List<Country>
+
+    @Query("SELECT * from Country WHERE continents LIKE :continent")
+    suspend fun getCountriesByContinent(continent: String): List<Country>
+
+    //insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(countries: List<Country>)
+
+    //delete
+    @Delete
+    suspend fun delete(country: Country): Int
+
+    //update
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateCountry(country: Country): Int
+
+    //Update2
+    @Query("UPDATE Country set capital = :capital where id = :id")
+    suspend fun updateCountry(capital: List<String>, id: Int): Int
+}
